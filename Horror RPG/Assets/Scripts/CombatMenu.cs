54c84dbc15cd7481bc0aa943723129d9a5499d7e
valor87 <anthonypattison0@gmail.com>
2025-11-06 22:18:39 -0500

    Creating project
    Added the project to the git hub

---
+++ Horror RPG/Assets/Scripts/CombatMenu.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class CombatMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public List<GameObject> CombatActions;
+    public List<GameObject> AttackActions;
+    GameObject Highlight;
+    GameObject ButtonHighlig;
+    public List<GameObject> CurrentMenu;
+    int pos = 0;
+
+    void Start()
+    {
+        CurrentMenu = CombatActions;
+        Highlight = GameObject.Find("ButtonGlow");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SelectActions(CurrentMenu);
+    }
+
+    void SelectActions(List<GameObject> CurrentMenu)
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            pos = CurrentMenu.Count-1;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            pos--;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            pos++;
+        }
+        if (pos <= 0)
+        {
+            pos = CurrentMenu.Count;
+        }
+        if (pos >= CurrentMenu.Count)
+        {
+            pos = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CurrentMenu[pos].GetComponent<Button>().onClick.Invoke();
+        }
+
+        Highlight.transform.position = CurrentMenu[pos].transform.position;
+    }
+    public void ToAttackMenu()
+    {
+        CurrentMenu = AttackActions;
+    }
+    public void BackToMainCombat()
+    {
+        CurrentMenu = CombatActions;
+    }
+}
