553e38e8837fc5e2b685369fb28c79eef8a730fc
valor87 <134859690+valor87@users.noreply.github.com>
2026-02-20 01:15:45 -0500

    camera moves when the player is on any other side of the screen

--- Horror RPG/Assets/Scripts/CameraScripts/CameraMovement.cs
+++ Horror RPG/Assets/Scripts/CameraScripts/CameraMovement.cs
@@ -2,28 +2,31 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+
     public Transform PlayerTransform;
+    public float CamSpeed;
     Vector3 CameraScreenSpace;
     float CameraRightSideBuffer;
     float CameraLeftSideBuffer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        CameraScreenSpace.x = Screen.width;
-        CameraScreenSpace.y = Screen.height;
-
-        CameraLeftSideBuffer = (CameraScreenSpace.x / 2) / 2;
-        CameraRightSideBuffer = CameraScreenSpace.x - (CameraScreenSpace.x / 4);
     }
 
     // Update is called once per frame
     void Update()
     {
+        print(Input.mousePosition);
         MoveCameraToFindPlayer(PlayerTransform);
     }
 
     void MoveCameraToFindPlayer(Transform PlayerTransform)
     {
+        CameraScreenSpace.x = Screen.width;
+        CameraScreenSpace.y = Screen.height;
+
+        CameraLeftSideBuffer = (CameraScreenSpace.x / 2) / 2;
+        CameraRightSideBuffer = (CameraScreenSpace.x / 2) + (CameraScreenSpace.x / 4);
         Vector3 PlayerInScreenSpace;
 
         PlayerInScreenSpace = Camera.main.WorldToScreenPoint(PlayerTransform.position);
@@ -31,10 +34,12 @@ public class CameraMovement : MonoBehaviour
 
         if (PlayerInScreenSpace.x < CameraLeftSideBuffer)
         {
+            transform.position -= new Vector3(CamSpeed,0,0) * Time.deltaTime;
             print("Left most side of the screen");
         }
         if (PlayerInScreenSpace.x > CameraRightSideBuffer)
         {
+            transform.position += new Vector3(CamSpeed, 0, 0) * Time.deltaTime;
             print("Right most side of the screen");
         }
     }
