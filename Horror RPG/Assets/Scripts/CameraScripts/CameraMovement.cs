9b11bd0ea533798f6260178d6289cf308b011b39
valor87 <134859690+valor87@users.noreply.github.com>
2026-02-21 20:39:13 -0500

    Adding more assets to the scene and fixing a bug with the camera

--- Horror RPG/Assets/Scripts/CameraScripts/CameraMovement.cs
+++ Horror RPG/Assets/Scripts/CameraScripts/CameraMovement.cs
@@ -11,6 +11,7 @@ public class CameraMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        transform.position = new Vector2(PlayerTransform.position.x, transform.position.y);
     }
 
     // Update is called once per frame
