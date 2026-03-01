17f46f5002b9fab79feed05dcfedf829efeffb79
valor87 <anthonypattison0@gmail.com>
2026-03-01 14:33:04 -0500

    adding a level up function to the characters scriptable objects

--- Horror RPG/Assets/Scriptable Objects/ScriptableObjectsCode/PlayerStats.cs
+++ Horror RPG/Assets/Scriptable Objects/ScriptableObjectsCode/PlayerStats.cs
@@ -24,10 +24,27 @@ public class PlayerStats : ScriptableObject
     public int Level = 1;
     [Tooltip("Sprite")]
     public Sprite Spriteimage;
-
+    [Header("Level up values for the player")]
+    [Tooltip("The amount of exsperirance for each level")]
+    [SerializeField]
+    int[] levelUpValues = new int[] {30,60,140,270,450,680,960,1290,1670,2100,2580};
     [Header("To be Changed during run time")]
     public bool WantsToRun;
-
+    [ContextMenu("Level up character")]
+    void processLevelUp()
+    {
+        float[] tempStatArray = new float[] {Healthstat,Attackstat,Defensestat,Speedstat};
+        int[] statChangeAmount = new int[] { 1, 2, 2, 3 };
+        for (int i = 0; i < tempStatArray.Length; i++)
+        {
+            tempStatArray[i] += statChangeAmount[Random.Range(0, statChangeAmount.Length)];
+            Debug.Log(tempStatArray[i]);
+        }
+        Healthstat = tempStatArray[0];
+        Attackstat = tempStatArray[1];
+        Defensestat = tempStatArray[2];
+        Speedstat = tempStatArray[3];
+    }
     private void OnEnable()
     {
         WantsToRun = false;
