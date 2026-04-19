245cfd537935b4a2c3b54e635920b9eb748b70a0
valor87 <anthonypattison0@gmail.com>
2026-04-18 23:15:39 -0400

    the selection knife now moves from selection to selection

--- Horror RPG/Assets/Scripts/newItemScripts/callChildButtons.cs
+++ Horror RPG/Assets/Scripts/newItemScripts/callChildButtons.cs
@@ -11,7 +11,8 @@ public class callChildButtons : MonoBehaviour
     public KeyCode upKey = KeyCode.UpArrow;
     public KeyCode downKey = KeyCode.DownArrow;
     public KeyCode invokeButton = KeyCode.Space;
-    
+    public RectTransform selectionKnife;
+    public Vector3 knifeOffset;
     int childIndex;
     void Update()
     {
@@ -28,13 +29,18 @@ public class callChildButtons : MonoBehaviour
         if (Input.GetKeyDown(invokeButton))
            invokeButtonGameObject = transform.GetChild(childIndex).gameObject;
 
-        childIndex = Mathf.Clamp(childIndex, 0, transform.childCount -1);
-
         if (invokeButtonGameObject != null)
         {
             invokeButtonGameObject.GetComponent<Button>().onClick.Invoke();
         }
-    
-    }
 
+        // visualy changing the knifes location
+        childIndex = Mathf.Clamp(childIndex, 0, transform.childCount - 1);
+        RectTransform currentButtonTransform = transform.GetChild(childIndex).gameObject.GetComponent<RectTransform>();
+        selectionKnifeLocation(currentButtonTransform);
+    }
+    void selectionKnifeLocation(RectTransform currentButton)
+    {
+        selectionKnife.position = currentButton.position + knifeOffset;
+    }
 }
--- Horror RPG/Assets/Scripts/newItemScripts/menuDisplay.cs
+++ Horror RPG/Assets/Scripts/newItemScripts/menuDisplay.cs
@@ -1,8 +1,12 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class menuDisplay : MonoBehaviour
 {
     EventCore eventCore;
+    public List<GameObject> subMenus = new List<GameObject>();
+    public KeyCode closeSubMenusKey = KeyCode.X;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -10,6 +14,20 @@ public class menuDisplay : MonoBehaviour
         eventCore.EV_OpenCloseMenu.AddListener(showMenu);
     }
 
+    private void Update()
+    {
+        closeAllSubMenus();
+    }
+    void closeAllSubMenus()
+    {
+        if (Input.GetKeyDown(closeSubMenusKey))
+        {
+            foreach (GameObject menu in subMenus)
+            {
+                menu.SetActive(false);
+            }
+        } 
+    }
     /// <summary>
     /// set and deactivate the overworld menu
     /// </summary>
